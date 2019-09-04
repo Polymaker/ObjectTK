a0cbc0206347980d4169f781905aa81743df1e4b
James William Turner <james_w_turner27@hotmail.com>
2019-09-04 13:41:54 -0400

    added Color4 support to Uniform

--- ObjectTK/Shaders/Variables/UniformSetter.cs
+++ ObjectTK/Shaders/Variables/UniformSetter.cs
@@ -13,6 +13,7 @@ using System.Drawing;
 using System.Linq;
 using ObjectTK.Exceptions;
 using OpenTK;
+using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
 namespace ObjectTK.Shaders.Variables
@@ -49,6 +50,7 @@ namespace ObjectTK.Shaders.Variables
                 new Map<double>(GL.Uniform1),
                 new Map<Half>((_, half) => GL.Uniform1(_, half)),
                 new Map<Color>((_, color) => GL.Uniform4(_, color)),
+                new Map<Color4>((_, color) => GL.Uniform4(_, color)),
                 new Map<Vector2>(GL.Uniform2),
                 new Map<Vector3>(GL.Uniform3),
                 new Map<Vector4>(GL.Uniform4),
